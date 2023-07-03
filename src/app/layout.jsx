6caf5df02dff09bac574c7bd830835fd6ac00030
tuanhiep <trinhtuanhiep1@gmail.com>
2023-07-03 22:53:54 +0700

    update tailwind

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 
-
+import './globals.css'
 export const metadata = {
   title: 'imdb',
   description: 'this is a imdb clone',
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-   <h1>home</h1>
+   <h1 className="text-3xl font-bold underline">home</h1>
   )
 }

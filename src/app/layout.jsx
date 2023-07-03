8b4c2a247d7d2e3efdc32699155b419e937bbdfd
tuanhiep <trinhtuanhiep1@gmail.com>
2023-07-03 22:33:27 +0700

    set-up

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+
+
+export const metadata = {
+  title: 'imdb',
+  description: 'this is a imdb clone',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}

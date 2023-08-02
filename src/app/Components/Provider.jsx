fc3f3867bb6ded90271eab0ce843bf4f9f962c1e
tuanhiep <trinhtuanhiep1@gmail.com>
2023-08-02 20:49:28 +0700

    aboutPage

--- src/app/Components/Provider.jsx
+++ src/app/Components/Provider.jsx
@@ -3,10 +3,8 @@ import { ThemeProvider } from 'next-themes'
 import React from 'react'
 export default function Provider({ children }) {
     return <ThemeProvider className enableSystem={true} attribute="class">
-        <div className='background'>
-        {children}
-        </div>
-        </ThemeProvider>
+            {children}
+    </ThemeProvider>
 
 }
 
